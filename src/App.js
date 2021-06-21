28891909e475ebe300d23f47e5a73a9ab8ebfc28
digeoesp <diegocrypto@outlook.com>
2021-06-21 16:56:28 -0700

    able to delete  task

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddTask from './components/AddTask'
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -26,6 +27,12 @@ function App() {
     },
 
   ])
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
   //delete task 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -41,8 +48,8 @@ function App() {
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks to display'}
     </div>
   );
