5ef9f100ab0be858eaa89f4de487d0f42a31d712
digeoesp <diegocrypto@outlook.com>
2021-06-21 00:00:33 -0700

    able to delete task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'doc appoint',
+      day: 'feb 5thcat 230',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'xsaxs',
+      day: 'fxasxsa30',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'xddacdacsasaxs',
+      day: 'fxscascsacascscsacsaasxsa30',
+      reminder: true,
+    },
+
+  ])
+  //delete task 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to display'}
     </div>
   );
 }
-
+//..filter high order array method
 export default App;
